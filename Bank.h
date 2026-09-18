8fa80fc2276f1072200282eaa4ca17c6ccf2118b
Jeffrey Chao <jefchao@amazon.com>
2026-09-18 09:25:24 -0700

    Add selector enum for users

--- Bank.h
+++ Bank.h
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <string>
 
+enum Option { DEPOSIT = 1, WITHDRAW, SEND, LOG_OUT, CLOSE_ACCOUNT };
+
 class Bank {
 private:
 public:
@@ -20,6 +22,8 @@ public:
               << ". What would you like to do today?" << std::endl;
 
     int option{};
+
+    enum Option selectedOption;
     do {
       std::cout << "1. Deposit money" << std::endl
                 << "2. Withdraw money" << std::endl
@@ -27,7 +31,8 @@ public:
                 << "4. Log out" << std::endl
                 << "5. Close Account" << std::endl;
       std::cin >> option;
-    } while (!inputIsValid(option));
+    } while (!inputIsValid(option, selectedOption));
+    executeOption(selectedOption);
   }
 
   std::string createAccount(std::string username, std::string password) {
@@ -72,16 +77,27 @@ public:
     }
   }
   void login(Person personLoggingIn) { user = personLoggingIn; }
-  bool inputIsValid(int option) {
+  bool inputIsValid(int option, Option &selectedOption) {
     switch (option) {
     case 1:
+      selectedOption = DEPOSIT;
+      return true;
     case 2:
+      selectedOption = WITHDRAW;
+      return true;
     case 3:
+      selectedOption = SEND;
+      return true;
     case 4:
+      selectedOption = LOG_OUT;
+
+      return true;
     case 5:
+      selectedOption = CLOSE_ACCOUNT;
       return true;
     default:
       return false;
     }
   }
+  void executeOption(Option option) { return; }
 };
